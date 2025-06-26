e5d3c386bd0d67d9cfab441b964cb6b935ea1a40
Puja Mehetre <puja.mehetre@harman.com>
2025-06-26 20:20:37 +0530

    Added demo.cpp and CMakeLists.txt, removed old files

---
+++ build/CMakeFiles/myApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myApp.dir/demo.cpp.o"
+  "CMakeFiles/myApp.dir/demo.cpp.o.d"
+  "myApp"
+  "myApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ demo.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main(){
+    std::cout<<"hello from jenkins";
+    std::cout<<"hello from puja";
+    return 0;
+}
